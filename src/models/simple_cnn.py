2f2132e9557c8af966843133233802d87ea0ae78
Lance H <lsaucyfx@gmail.com>
2024-02-23 09:28:20 -0500

    defined CNN model

---
+++ src/models/simple_cnn.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+class SimpleCNN(nn.Module):
+    def __init___(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 8 * 8)
+        x = self.fc2(x)
+        return x
+
+model = SimpleCNN()
+
+
+
